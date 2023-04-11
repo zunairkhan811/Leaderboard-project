0346746beb9b2a015c7843497123f2c6ad6c7d8b
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-04-11 20:40:20 +0500

    Add html and styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import "./styles.css";
+
+const scoreList = document.querySelector('.score-list');
+const form = document.querySelector('#form');
+const name = document.querySelector('#name');
+const score = document.querySelector('#score');
+const formbtn = document.querySelector('.form-btn');
+
+if(scoreList.textContent.trim() === ''){
+    scoreList.innerHTML = ` <div class="score-items">
+    <p>Ali: 100</p>
+</div>
+<div class="score-items">
+    <p>Aslam: 20</p>
+</div>
+<div class="score-items">
+    <p>Akram: 30</p>
+</div>`
+}
+    
+    formbtn.addEventListener('click',(e)=>{
+        e.preventDefault();
+        e.stopPropagation();
+        let namevalue = name.value;
+        let scorevalue = score.value;
+        console.log(namevalue)
+        console.log(scorevalue)
+        scoreList.innerHTML += ` <div class="score-items">
+        <p>${namevalue}: ${scorevalue}</p>
+    </div>`
+     })
+
+
+  
