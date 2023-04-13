269eca015cba377ba7f569a284f11619dfdd886b
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-04-13 10:53:03 +0500

    Make changes in js codeand remove linter errors

--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
 import './styles.css';
-import receiveList from '../modules/getDataFromApi';
+import receiveList from '../modules/getdata.js';
 
-const scoreList = document.querySelector('.score-list');
 const name = document.querySelector('#name');
 const score = document.querySelector('#score');
 const formbtn = document.querySelector('.form-btn');
 const form = document.querySelector('#form');
 const refreshbtn = document.querySelector('.recent-btn');
+const scoreList = document.querySelector('.score-list');
 
 formbtn.addEventListener('click', (e) => {
   e.preventDefault();
   e.stopPropagation();
-  let namevalue = name.value;
-  let scorevalue = score.value;
+  const namevalue = name.value;
+  const scorevalue = score.value;
   fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/kabirmybaby/scores/', {
-  method: 'POST',
-  body: JSON.stringify({
-    "user":namevalue,
-    "score":scorevalue
-  }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-   form.reset();
+    method: 'POST',
+    body: JSON.stringify({
+      user: namevalue,
+      score: scorevalue,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json());
+  form.reset();
 });
 
-refreshbtn.addEventListener('click',receiveList.runList)
-
-
+refreshbtn.addEventListener('click', () => {
+  receiveList.getData().then((data) => {
+    const addData = data.result;
+    scoreList.innerHTML = addData.map((cgame) => ` <div class="score-items">
+     <p>${cgame.user}: ${cgame.score}</p>
+     </div>`).join('');
+  });
+  receiveList.getData().catch(() => { 'Error occured in bringing data'; });
+});
